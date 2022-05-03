e64ec9a74b2ba5db7a894ca9ca838f7403ee81b4
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-03 14:28:58 +0200

    Creato files sass e avviato creazione dei componenti

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Primo Componente -->
+    <div id="site_header">
+
+    </div>
+    <!-- Secondo Componente -->
+    <div id="site_content">
+
+    </div>
+    <!-- Terzo Componente -->
+    <div id="site_main">
+
+    </div>
+    <!-- Quarto Componente -->
+    <div id="upper_footer">
+
+    </div>
+    <!-- Quinto Componente -->
+    <div id="under_footer">
+
+    </div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+// Qui do stile ma poi lo sposto nei componenti singoli
+
+
+
 </style>
