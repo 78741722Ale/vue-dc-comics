4cb1ebeca2322cba572b609e6e3481bc7723eb28
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-03 15:37:57 +0200

    Creata struttura base su app.vue, preparazione dei componenti

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,47 @@
 <template>
   <div id="app">
     <!-- Primo Componente -->
-    <div id="site_header">
-
+    <div class="h_header w-100 bg-info bordo" id="site_header">
+      <!-- Container principale -->
+      <div class="container h-100">
+        <div class="row m-0 w-100 bordo h-100 bg-danger"></div>
+      </div>
     </div>
     <!-- Secondo Componente -->
-    <div id="site_content">
-
+    <div class="h_content bordo w-100 bg-info" id="site_content">
+      <!-- Container principale -->
+      <div class="container h-100">
+        <div class="row m-0 w-100 bordo h-100 bg-danger">
+          <h3>| Content Goes Here | </h3>
+        </div>
+      </div>
     </div>
     <!-- Terzo Componente -->
-    <div id="site_main">
-
+    <div class="h_main bordo w-100 bg-info" id="site_main">
+      <!-- Container principale -->
+      <div class="container h-100">
+        <div class="row m-0 w-100 bordo h-100 bg-danger">
+          <h3>Qui 5 Col </h3>
+        </div>
+      </div>
     </div>
-    <!-- Quarto Componente -->
-    <div id="upper_footer">
-
+    <!-- Quarto Componente - -->
+    <div class="h_upper_footer bordo w-100 bg-info" id="upper_footer">
+      <!-- Container principale -->
+      <div class="container h-100">
+        <div class="row m-0 w-100 bordo h-100 bg-danger">
+          <h3>Qui tutto il footer superiore </h3>
+        </div>
+      </div>
     </div>
-    <!-- Quinto Componente -->
-    <div id="under_footer">
-
+    <!-- Quinto Componente - Footer Inferiori -->
+    <div class="h_under_footer bordo w-100 bg-info" id="under_footer">
+      <!-- Container principale -->
+      <div class="container h-100">
+        <div class="row m-0 w-100 bordo h-100 bg-danger">
+          <h3>Qui tutto il footer inferiore </h3>
+        </div>
+      </div>
     </div>
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
@@ -27,6 +50,7 @@
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
@@ -37,16 +61,57 @@ export default {
 </script>
 
 <style lang="scss">
+// Common rules 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.bordo {
+  border: 2px solid black;
+}
+// Main container (equivale al body)
 #app {
   font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+// Qui do stile ma poi lo sposto nei componenti singoli
+// Per ora diviso in region, poi da spostare in partials
+
+/*#region Header */
+.h_header {
+  height: 100px;
+}
+/*#endregion */
+
+/*#region Content */
+.h_content {
+  height: 130px;
+}
+/*#endregion */
+
+/*#region Main */
+.h_main {
+  height: 155px;
 }
 
-// Qui do stile ma poi lo sposto nei componenti singoli
+/*#endregion */
+
+/*#region Upper Footer */
+.h_upper_footer {
+  height: 370px;
+}
+/*#endregion */
+
+/*#region Under Footer */
+.h_under_footer {
+  height: 100px;
+}
+
+/*#endregion */
 
 
 
